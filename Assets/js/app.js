301368553a7d753b34c0038cccbf026b67ce89bb
Jacob Adamson <jakeladamson@gmail.com>
2021-08-05 17:42:01 -0600

    started blocks, haven't finished editing times

---
+++ Assets/js/app.js
@@ -0,0 +1,16 @@
+// declare variables
+
+//create functions
+
+//pseudo code
+//when planner is opened, current day is displayed at the top of calendar
+
+//Then upon scroll down, I am presented with timeblocks for standard biz hours
+
+//when timeblocks are viewed for that day, each timebloc is color coded to indicate whether is is in the future
+
+//When I click into timeblock, then i Enter and event
+
+//When I slick the save btn, then the text for that event is saved in loacl stoarge
+
+//When i refresh the page, then the saved events persist (local storage)
